8d03c6f763570ae5a0a55319b9a897ca8975dc25
georgeravensm <34068912+georgeravensm@users.noreply.github.com>
2018-10-09 17:14:32 -0400

    Update Neighbor_Network_Test.cpp

--- Neighbor_Network_Test.cpp
+++ Neighbor_Network_Test.cpp
@@ -4,12 +4,9 @@ using namespace std;
 #include "Neighbor_Network.h"
 
 int main() {
-  const int SIZE = 9;
-  string res_halls[SIZE] = {"Baldwin Hall","Blount Towers", "Cummings House", "Harper-Tubman House", "Marble Hall Gardens Apartments", "O'Connell Hall", "Rawlings Hall", "Thurgood Marshall Apartments", "Morgan View"};
-
   string SignOrSearch;
 	cout<<  "Hello, Welcome to the Neighbor Network! Would you like to Sign Up or Search for an item?";
-	cin >> SignOrSearch;
+	getline(cin, SignOrSearch);
   Info user;
   if (SignOrSearch == "sign up" || SignOrSearch == "Sign Up" || SignOrSearch == "Sign up" || SignOrSearch == "sign Up"){
     sign_up();
