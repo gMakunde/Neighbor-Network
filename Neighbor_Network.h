e6a8f7bd3c8d25083653feff1a6de9e32f7010d8
georgeravensm <34068912+georgeravensm@users.noreply.github.com>
2018-10-09 15:51:15 -0400

    Create Neighbor_Network.h
    separating headers into a different file

---
+++ Neighbor_Network.h
@@ -0,0 +1,17 @@
+#ifndef NEIGHBOR_NETWORK_H
+#define NEIGHBOR_NETWORK_H
+using namespace std;
+#include <string>
+string standard_dorm_name(string);
+void sign_up();
+class Info {
+    string residence;
+    string name;
+    string email;
+    string number;
+    string item;
+};
+
+
+
+#endif
