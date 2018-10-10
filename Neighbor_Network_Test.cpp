5062c7a3fdb0f74608903e17ee59b977e0795130
georgeravensm <34068912+georgeravensm@users.noreply.github.com>
2018-10-10 17:03:56 -0400

    Update Neighbor_Network_Test.cpp

--- Neighbor_Network_Test.cpp
+++ Neighbor_Network_Test.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int main() {
   string SignOrSearch;
-	cout<<  "Hello, Welcome to the Neighbor Network! Would you like to Sign Up or Search for an item?";
+	cout<<  "Hello, Welcome to the Neighbor Network! Would you like to Sign Up or Search for an item? ";
 	getline(cin, SignOrSearch);
   transform(SignOrSearch.begin(), SignOrSearch.end(), SignOrSearch.begin(), ::tolower);
   if (SignOrSearch == "sign up"){
     sign_up();
     string yesOrno;
     cout << "Thanks for Signing up with the Neighbor Network!" << endl;
-    cout << "Would you Like to search the Neighbor Network? (y/n)";
+    cout << "Would you Like to search the Neighbor Network? (y/n) ";
     cin >> yesOrno;
     transform(yesOrno.begin(), yesOrno.end(), yesOrno.begin(), ::tolower);
     if (yesOrno == "yes" || yesOrno == "y"){
@@ -25,20 +25,20 @@ int main() {
   } 
   if (SignOrSearch == "search"){
     string dorm;
-    cout<< "Enter The dorm you would like to search or enter Campus-wide";
+    cout<< "Enter The dorm you would like to search or enter Campus-wide? ";
     cin >> dorm;
     dorm = standard_dorm_name(dorm);
     string item;
-    cout << "enter the name of the item you are searching for";
-    cin >> item;
-    void search();
+    cout << "enter the name of the item you are searching for ";
+    getline(cin, item);
+    void search(dorm, item);
 
   }
   if (SignOrSearch == "end"){
     cout << "okay, have a great day!" << endl;
   }
   else{
-    cout << "Sorry that is not a valid choice :/" << endl;
+    cout << "Sorry that is not a valid choice :/. Please Try again" << endl;
     main();
   }
 
@@ -46,5 +46,3 @@ int main() {
 
 return 0;
 }
-
-
