f4b2718c0a2a943817e52ce05b95c337faf63f8a
georgeravensm <34068912+georgeravensm@users.noreply.github.com>
2018-10-11 05:54:48 -0400

    Update Neighbor_Network.h

--- Neighbor_Network.h
+++ Neighbor_Network.h
@@ -2,18 +2,22 @@
 #define USER_H
 using namespace std;
 #include <string>
+#include <vector>
 
 string standard_dorm_name(string);
 void sign_up();
 void search(string, string);
+string ExePath();
+void GetFilenames(vector<string>&);
 
 struct user {
-  string residence;
-  string name;
-  string email;
-  string number;
-  int items_len;
-  
+	string residence;
+	string name;
+	string email;
+	string number;
+	string items;
+	int items_len;
+
 };
 
 
